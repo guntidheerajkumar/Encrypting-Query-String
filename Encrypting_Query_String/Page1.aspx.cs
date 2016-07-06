878f4b3a20f87e418e4e980240d2d709af1fe91b
Dheeraj Kumar Gunti <guntidheerajkumar@gmail.com>
2016-07-06 16:19:31 +0530

    Encrypting Query String

---
+++ Encrypting_Query_String/Page1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Encrypting_Query_String
+{
+    public partial class Page1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void _BtnRedirect_Click(object sender, EventArgs e)
+        {
+            EncryptedQueryString args = new EncryptedQueryString();
+            args["arg1"] = "val1";
+            args["arg2"] = "val2";
+            Response.Redirect(String.Format("Page2.aspx?args={0}", args.ToString()));
+        }
+    }
+}
---
+++ Encrypting_Query_String/Page2.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Encrypting_Query_String
+{
+    public partial class Page2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            EncryptedQueryString args = new EncryptedQueryString(Request.QueryString["args"]);
+            Response.Write(string.Format("arg1={0}, arg2={1}", args["arg1"], args["arg2"]));
+        }
+    }
+}
